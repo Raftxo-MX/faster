3a62c3dd6ec5897cc5fd1508a9f4632de8a3c6f5
Rafael <115911092+Raftxo-MX@users.noreply.github.com>
2023-07-06 16:48:30 +0200

    Minor updates to main.py

--- main.py
+++ main.py
@@ -16,7 +16,56 @@ df = pd.read_csv('movie_dataset_clean.csv')
 
 @app.get('/')
 def index():   
-    return {'message': 'PI01 MLOPS, alumno : Alex Ezequiel Dalpiaz'}
+    return {'message': '尺闩ﾁ七〤ㄖ ﾁ闩丂セ 闩尸讠',
+           'description': 'Implementación en Render de la API con FastAPI',
+          'organization': 'soyHENRY.com',
+               'carreer': 'soyHenry bootcamp DATA SCIENCE',
+               'student': 'Rafal Janusz Wysocki',
+           'mail/github': 'raftxo.mx@gmail.com', 
+           }
+
+
+@app.get('/peliculas_idioma/{idioma}')
+def peliculas_idioma(idioma:str):
+    '''Ingresas el idioma, retornando la cantidad de peliculas producidas en el mismo'''
+    return {'idioma':idioma, 'cantidad':respuesta}
+    
+@app.get('/peliculas_duracion/{pelicula}')
+def peliculas_duracion(pelicula:str):
+    '''Ingresas la pelicula, retornando la duracion y el año'''
+    return {'pelicula':pelicula, 'duracion':respuesta, 'anio':anio}
+
+@app.get('/franquicia/{franquicia}')
+def franquicia(franquicia:str):
+    '''Se ingresa la franquicia, retornando la cantidad de peliculas, ganancia total y promedio'''
+    return {'franquicia':franquicia, 'cantidad':respuesta, 'ganancia_total':respuesta, 'ganancia_promedio':respuesta}
+
+@app.get('/peliculas_pais/{pais}')
+def peliculas_pais(pais:str):
+    '''Ingresas el pais, retornando la cantidad de peliculas producidas en el mismo'''
+    return {'pais':pais, 'cantidad':respuesta}
+
+@app.get('/productoras_exitosas/{productora}')
+def productoras_exitosas(productora:str):
+    '''Ingresas la productora, entregandote el revunue total y la cantidad de peliculas que realizo '''
+    return {'productora':productora, 'revenue_total': respuesta,'cantidad':respuesta}
+
+
+@app.get('/get_director/{nombre_director}')
+def get_director(nombre_director:str):
+    ''' Se ingresa el nombre de un director que se encuentre dentro de un dataset debiendo devolver el éxito del mismo medido a través del retorno. 
+    Además, deberá devolver el nombre de cada película con la fecha de lanzamiento, retorno individual, costo y ganancia de la misma. En formato lista'''
+    return {'director':nombre_director, 'retorno_total_director':respuesta, 
+    'peliculas':respuesta, 'anio':respuesta,, 'retorno_pelicula':respuesta, 
+    'budget_pelicula':respuesta, 'revenue_pelicula':respuesta}
+
+# ML
+@app.get('/recomendacion/{titulo}')
+def recomendacion(titulo:str):
+    '''Ingresas un nombre de pelicula y te recomienda las similares en una lista'''
+    return {'lista recomendada': respuesta}
+
+# =====================
 
 
 @app.get('/peliculas_mes/{mes}')
